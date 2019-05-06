d48f02cbefd767c6382ff0ef36b20b44a0500ca2
Takuto Yanagida <yanagida@stxst.com>
2019-05-06 12:01:42 +0900

    Improve comments

--- src/_edge.js
+++ src/_edge.js
@@ -1,27 +1,66 @@
-// -------------------------------------------------------------------------
-// エッジ生成関数
-// -------------------------------------------------------------------------
-
-
+/**~ja
+ * エッジ生成関数
+ * @author Takuto Yanagida
+ * @version 2019-05-06
+ */
+/**~en
+ * Edge generation functions
+ * @author Takuto Yanagida
+ * @version 2019-05-06
+ */
 
 
 const NORMAL_EDGE = null;
 
-// 直線のエッジを作る
+
+/**~ja
+ * 直線のエッジを作る
+ * @return {function(number, number): number} 直線のエッジ
+ */
+/**~en
+ * Make a straight edge
+ * @return {function(number, number): number} Straight edge
+ */
 const normalEdge = function () {
 	return NORMAL_EDGE;
 };
 
-// サイン波のエッジを作る（<長さ>、<振幅>、<オプション>）
+/**~ja
+ * サイン波のエッジを作る
+ * @param {number=} [length=10] 長さ
+ * @param {number=} [amplitude=10] 振幅
+ * @param {*=} [opt={}] オプション
+ * @return {function(number, number): number} サイン波のエッジ
+ */
+/**~en
+ * Make a sine wave edge
+ * @param {number=} [length=10] Length
+ * @param {number=} [amplitude=10] Amplitude
+ * @param {*=} [opt={}] Options
+ * @return {function(number, number): number} Sine wave edge
+ */
 const sineEdge = function (length = 10, amplitude = 10, opt = {}) {
-	return makeEdge(length, amplitude, opt, function (p) {
+	return _makeEdge(length, amplitude, opt, function (p) {
 		return Math.sin(p * Math.PI * 2);
 	}, -0.25, 0.25);
 };
 
-// 矩形波のエッジを作る（<長さ>、<振幅>、<オプション>）
+/**~ja
+ * 矩形波のエッジを作る
+ * @param {number=} [length=10] 長さ
+ * @param {number=} [amplitude=10] 振幅
+ * @param {*=} [opt={}] オプション
+ * @return {function(number, number): number} 矩形波のエッジ
+ */
+/**~en
+ * Create a square wave edge
+ * @param {number=} [length=10] Length
+ * @param {number=} [amplitude=10] Amplitude
+ * @param {*=} [opt={}] Options
+ * @return {function(number, number): number} Square wave edge
+ */
 const squareEdge = function (length = 10, amplitude = 10, opt = {}) {
-	return makeEdge(length, amplitude, opt, function (p) {
+	return _makeEdge(length, amplitude, opt, function (p) {
 		if (p < 0.01) return p / 0.01;
 		if (p < 0.49) return 1;
 		if (p < 0.51) return -(p - 0.5) / 0.01;
@@ -30,33 +69,93 @@ const squareEdge = function (length = 10, amplitude = 10, opt = {}) {
 	}, -0.01, 0.01);
 };
 
-// 三角波のエッジを作る（<長さ>、<振幅>、<オプション>）
+/**~ja
+ * 三角波のエッジを作る
+ * @param {number=} [length=10] 長さ
+ * @param {number=} [amplitude=10] 振幅
+ * @param {*=} [opt={}] オプション
+ * @return {function(number, number): number} 三角波のエッジ
+ */
+/**~en
+ * Create a triangle wave edge
+ * @param {number=} [length=10] Length
+ * @param {number=} [amplitude=10] Amplitude
+ * @param {*=} [opt={}] Options
+ * @return {function(number, number): number} Triangle wave edge
+ */
 const triangleEdge = function (length = 10, amplitude = 10, opt = {}) {
-	return makeEdge(length, amplitude, opt, function (p) {
+	return _makeEdge(length, amplitude, opt, function (p) {
 		if (p < 0.25) return p / 0.25;
 		if (p < 0.75) return -(p - 0.5) / 0.25;
 		return (p - 1) / 0.25;
 	}, -0.25, 0.25);
 };
 
-// のこぎり波のエッジを作る（<長さ>、<振幅>、<オプション>）
+/**~ja
+ * のこぎり波のエッジを作る
+ * @param {number=} [length=10] 長さ
+ * @param {number=} [amplitude=10] 振幅
+ * @param {*=} [opt={}] オプション
+ * @return {function(number, number): number} のこぎり波のエッジ
+ */
+/**~en
+ * Create a sawtooth wave edge
+ * @param {number=} [length=10] Length
+ * @param {number=} [amplitude=10] Amplitude
+ * @param {*=} [opt={}] Options
+ * @return {function(number, number): number} Sawtooth wave edge
+ */
 const sawtoothEdge = function (length = 10, amplitude = 10, opt = {}) {
-	return makeEdge(length, amplitude, opt, function (p) {
+	return _makeEdge(length, amplitude, opt, function (p) {
 		if (p < 0.49) return p / 0.49;
 		if (p < 0.51) return -(p - 0.5) / 0.01;
 		return (p - 1) / 0.49;
 	}, -0.49, 0.49);
 };
 
-// サインの絶対値の波形のエッジを作る（<長さ>、<振幅>、<オプション>）
+/**~ja
+ * サインの絶対値の波形のエッジを作る
+ * @param {number=} [length=10] 長さ
+ * @param {number=} [amplitude=10] 振幅
+ * @param {*=} [opt={}] オプション
+ * @return {function(number, number): number} サイン波のエッジ
+ */
+/**~en
+ * Make an edge of the absolute value of the sine function
+ * @param {number=} [length=10] Length
+ * @param {number=} [amplitude=10] Amplitude
+ * @param {*=} [opt={}] Options
+ * @return {function(number, number): number} Edge of the absolute value of the sine function
+ */
 const absSineEdge = function (length = 10, amplitude = 10, opt = {}) {
-	return makeEdge(length, amplitude, opt, function (p) {
+	return _makeEdge(length, amplitude, opt, function (p) {
 		return Math.abs(Math.sin(p * Math.PI + Math.PI / 6)) * 2 - 1;
 	}, -1 / 6, 1 / 3);
 };
 
-// （ライブラリ内だけで使用）エッジを作る（長さ、振幅、オプション、関数（原点を通り振幅±1）、最小値フェーズ、最大値フェーズ）
-const makeEdge = function (length, amplitude, opt, func, minPhase, maxPhase) {
+/**~ja
+ * エッジを作る
+ * @private
+ * @param {number} length 長さ
+ * @param {number} amplitude 振幅
+ * @param {*} opt オプション
+ * @param {function(number): number} func 関数（原点を通り振幅±1）
+ * @param {number} minPhase 最小値フェーズ
+ * @param {number} maxPhase 最大値フェーズ
+ * @return {function(number, number): number} エッジ
+ */
+/**~en
+ * Make an edge
+ * @private
+ * @param {number} length Length
+ * @param {number} amplitude Amplitude
+ * @param {*} opt Options
+ * @param {function(number): number} fn Function (Amplitude ± 1 through the origin)
+ * @param {number} minPhase Minimum phase
+ * @param {number} maxPhase Maximum phase
+ * @return {function(number, number): number} Edge
+ */
+const _makeEdge = function (length, amplitude, opt, fn, minPhase, maxPhase) {
 	let amp = 0.5 * amplitude;
 	let phase = minPhase, off = 1, rev = 1;
 
@@ -75,6 +174,6 @@ const makeEdge = function (length, amplitude, opt, func, minPhase, maxPhase) {
 		const l = max / Math.max(1, (~~(max / length)));
 		let p = x % l / l + phase;
 		if (p < 0) p += 1;
-		return (func(p) * rev + off) * amp;
+		return (fn(p) * rev + off) * amp;
 	};
 };
