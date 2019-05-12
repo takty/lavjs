e1e427421180457a234df2c52a92f07f7734eb6f
Takuto Yanagida <yanagida@stxst.com>
2019-05-12 23:29:38 +0900

    Improve comments

--- src/_command.js
+++ src/_command.js
@@ -1,17 +1,38 @@
-// -------------------------------------------------------------------------
-// コマンド (TRACER.Tracer)
-// -------------------------------------------------------------------------
-
-
+/**~ja
+ * コマンド
+ * @version 2019-05-12
+ */
+/**~en
+ * Command
+ * @version 2019-05-12
+ */
 class Command {
 
-	// （ライブラリ内だけで使用）コマンドを作る（関数）
+	/**~ja
+	 * コマンドを作る（ライブラリ内だけで使用）
+	 * @private
+	 * @param {function} func 関数
+	 */
+	/**~en
+	 * Make a command (used only in the library)
+	 * @private
+	 * @param {function} func Function
+	 */
 	constructor(func) {
 		this._func = func;
 		this._initState = null;
 	}
 
-	// （ライブラリ内だけで使用）コマンドを実行する（進める時間）
+	/**~ja
+	 * コマンドを実行する（ライブラリ内だけで使用）
+	 * @param {number} deltaT 進める時間
+	 * @return {number} パワー消費
+	 */
+	/**~en
+	 * Run the command (used only in the library)
+	 * @param {number} deltaT Time to advance
+	 * @return {number} Power consumption
+	 */
 	run(deltaT) {
 		const pc = this._func(deltaT);
 		return (pc === undefined) ? 0 : pc;
