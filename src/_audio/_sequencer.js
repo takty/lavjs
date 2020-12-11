5c606a4faca4d1f03a3a0f72348e6d75890897ef
takty <tokutadaginaya@gmail.com>
2020-12-12 01:48:28 +0900

    Implementing

--- src/_audio/_sequencer.js
+++ src/_audio/_sequencer.js
@@ -1,10 +1,10 @@
 /**~ja
  * シーケンサー
- * @version 2020-12-05
+ * @version 2020-12-11
  */
 /**~en
  * Sequencer
- * @version 2020-12-05
+ * @version 2020-12-11
  */
 class Sequencer {
 
@@ -21,7 +21,7 @@ class Sequencer {
 	}
 
 	constructor(context, params) {
-		this._scheduler = new Scheduler(context);
+		this._scheduler = new Scheduler(() => { return context.currentTime; });
 		this._lastTime = this._scheduler.now() + 1;
 
 		params = normalizeParams(params);
@@ -60,6 +60,98 @@ class Sequencer {
 		this._lastTime += dur;
 		this._scheduler.start();
 		return this;
+	}
+
+	playNote(notes) {
+		const ni = { 'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11 };
+		let bpm = this._bpm;
+		let oct = 4;
+		let shift = 0;
+		let len = 4;
+		let vel = 5;
+		let l = 0;
+		const numRe = new RegExp('\d+');
+		const lenRe = new RegExp('\.|\d+\.?');
+		for (let i = 0; i < notes.length; i += 1) {
+			const c = notes[i];
+			switch (c) {
+				case 'C': case 'D': case 'E': case 'F': case 'G': case 'A': case 'B':
+					const nn = ni[c] + (oct + 1) * 12 + shift;
+					shift = 0;
+					[l, i] = this._getLength(notes, i + 1, len);
+					console.log(c, nn, l);
+					break;
+				case '#': case '+':
+					shift += 1;
+					break;
+				case '-':
+					shift -= 1;
+					break;
+				case 'R':
+					[l, i] = this._getLength(notes, i + 1, len);
+					console.log(c, l);
+					break;
+				case 'L':
+					[len, i] = this._getLength(notes, i + 1, len);
+					console.log('LEN', len);
+					break;
+				case 'O':
+					[oct, i] = this._getNum(notes, i + 1, oct);
+					console.log('OCT', oct);
+					break;
+				case 'V':
+					[vel, i] = this._getNum(notes, i + 1, vel);
+					console.log('VEL', vel);
+					break;
+				case 'T':
+					[bpm, i] = this._getNum(notes, i + 1, bpm);
+					console.log('BPM', bpm, i);
+					break;
+				case '>':
+					oct += 1;
+					break;
+				case '<':
+					oct -= 1;
+					break;
+			}
+		}
+	}
+
+	_getNum(str, idx, def) {
+		const re = /\d+/y;
+		re.lastIndex = idx;
+		const res = re.exec(str);
+		if (res === null) return [def, idx];
+		const v = parseInt(res[0]);
+		const i = re.lastIndex - 1;
+		return [v, i];
+	}
+
+	_getLength(str, idx, def) {
+		const re = /\.+|(\d+)(\.*)/y;
+		re.lastIndex = idx;
+		const res = re.exec(str);
+		if (res === null) return [def, idx];
+		let l = def
+		if (!res[1]) {
+			let hl = l / 2;
+			for (let i = 0; i < res[0].length; i += 1) {
+				l += hl;
+				hl /= 2;
+			}
+		} else {
+			const v = parseInt(res[1]);
+			l = (4 * (60 / this._bpm) * (1 / v));
+			if (res[2]) {
+				let hl = l / 2;
+				for (let i = 0; i < res[2].length; i += 1) {
+					l += hl;
+					hl /= 2;
+				}
+			}
+		}
+		const i = re.lastIndex - 1;
+		return [l, i];
 	}
 
 	playBeat(beats) {
