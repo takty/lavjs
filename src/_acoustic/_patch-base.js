8570c0bc14e2be1ee58b09dab1c432be0f06279d
takty <tokutadaginaya@gmail.com>
2020-12-16 23:47:12 +0900

    Remove factory method

--- src/_acoustic/_patch-base.js
+++ src/_acoustic/_patch-base.js
@@ -9,42 +9,6 @@
 class Patch {
 
 	/**~ja
-	 * 各種パッチを作る
-	 * @param {Synth} synth シンセ
-	 * @param {string} type パッチの種類
-	 * @param {object} params パラメーター
-	 * @return {?Patch} パッチ
-	 */
-	/**~en
-	 * Make various patches
-	 * @param {Synth} synth Synth
-	 * @param {string} type Type of a patch
-	 * @param {object} params Parameters
-	 * @return {?Patch} A patch
-	 */
-	static make(synth, type, params) {
-		type = KEY_NORM_LIST[type] ?? type;
-		params = normalizeParams(params);
-		params.type = type;
-
-		switch (type) {
-			case 'sine': case 'triangle': case 'sawtooth': case 'square':
-			case 'oscillator': return new PATCH.OscillatorPatch(synth, params);
-			case 'microphone': return new PATCH.MicrophonePatch(synth, params);
-			case 'noise'     : return new PATCH.NoisePatch(synth, params);
-			case 'file'      : return new PATCH.BufferSourcePatch(synth, params);
-
-			case 'gain'      : return new PATCH.GainPatch(synth, params);
-			case 'lowpass': case 'highpass': case 'bandpass': case 'lowshelf': case 'highshelf': case 'peaking': case 'notch': case 'allpass':
-			case 'biquad'   : return new PATCH.BiquadFilterPatch(synth, params);
-
-			case 'scope'   : return new PATCH.ScopePatch(synth, params);
-			case 'envelope': return new PATCH.EnvelopePatch(synth, params);
-		}
-		throw `Cannot make '${type}' patch!`;
-	}
-
-	/**~ja
 	 * パッチを作る
 	 * @param {Synth} synth シンセ
 	 */
